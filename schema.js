c955e9086934e90cf402f24f1c057dbd3b48d8a7
Jeffrey Jones <jeffrey.jones@Levvels-MBP.lan1>
2021-09-20 11:37:44 -0400

    added offset-based pagination

--- schema.js
+++ schema.js
@@ -1,99 +1,110 @@
-const graphql = require('graphql')
-const axios = require('axios').default;
+const graphql = require("graphql");
+const axios = require("axios").default;
 const {
-    GraphQLString,
-    GraphQLInt,
-    GraphQLList,
-    GraphQLObjectType,
-    GraphQLSchema
-} = graphql
-
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+} = graphql;
 
 const MenuItem = new GraphQLObjectType({
-    name: 'MenuItemType',
-    fields: () => ({
-        id: {type: GraphQLString},
-        name:{type: GraphQLString},
-        price: {type: GraphQLString}
-    })
+  name: "MenuItemType",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    price: { type: GraphQLString },
+  }),
 });
 
 const Restaurant = new GraphQLObjectType({
-    name: 'RestaurantType',
-    fields: ()=>({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        email: {type: GraphQLString},
-        location: {type: GraphQLString},
-        menu: {type: GraphQLList(MenuItem)}
-    })
+  name: "RestaurantType",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    location: { type: GraphQLString },
+    menu: { type: GraphQLList(MenuItem) },
+  }),
 });
 
 const Customer = new GraphQLObjectType({
-    name: 'UserType',
-    fields: () => ({
-        id: {type: GraphQLString},
-        name: {type: GraphQLString},
-        email: {type: GraphQLString},
-        location: {type: GraphQLString}
-    })
+  name: "UserType",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    location: { type: GraphQLString },
+  }),
 });
 
 const Order = new GraphQLObjectType({
-    name: 'OrderType',
-    fields: () => ({
-        id: {type: GraphQLString},
-        customerId: {type: GraphQLString},
-        restaurantId: {type: GraphQLString},
-        order: {type: GraphQLList(GraphQLString)}
-    })
-})
+  name: "OrderType",
+  fields: () => ({
+    id: { type: GraphQLString },
+    customerId: { type: GraphQLString },
+    restaurantId: { type: GraphQLString },
+    order: { type: GraphQLList(GraphQLString) },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        restaurants: {
-            type: GraphQLList(Restaurant),
-            resolve (parentValue, args) {
-                return axios.get(`http://localhost:4200/restaurants`)
-                    .then(res => res.data);
-            }
-        },
-        restaurant: {
-            type: Restaurant,
-            args: {
-                id: {type: GraphQLString}
-                
-            },
-            resolve (parentValue, args){
-                return axios.get(`http://localhost:4200/restaurants/${args.id}`).then(res => res.data)
-            }
-        },
-        customers: {
-            type: GraphQLList(Customer),
-            resolve(parentValue, args){
-                return axios.get(`http://localhost:4200/customers`).then(res => res.data);
-            }
-        },
-        customer:{
-            type: Customer,
-            args: {
-                id: {type: GraphQLString}
-            },
-            resolve(parentValue, args){
-                return axios.get(`http://localhost:4200/customers/${args.id}`).then(res => res.data);
-            }
-        },
-        orders: {
-            type: GraphQLList(Order),
-            resolve (parentValue, args){
-                return axios.get('http://localhost:4200/orders').then(res => res.data)
-            }
-        }
-        
-    }
+  name: "RootQueryType",
+  fields: {
+    restaurants: {
+      type: GraphQLList(Restaurant),
+      args: { first: { type: GraphQLInt }, offset: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return axios
+          .get(
+            `http://localhost:4200/restaurants?_start=${args.offset}&_end=${
+              args.first + args.offset
+            }`
+          )
+          .then((res) => res.data);
+      },
+    },
+    restaurant: {
+      type: Restaurant,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:4200/restaurants/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+    customers: {
+      type: GraphQLList(Customer),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:4200/customers`)
+          .then((res) => res.data);
+      },
+    },
+    customer: {
+      type: Customer,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:4200/customers/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+    orders: {
+      type: GraphQLList(Order),
+      resolve(parentValue, args) {
+        return axios
+          .get("http://localhost:4200/orders")
+          .then((res) => res.data);
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
-})
+  query: RootQuery,
+});
